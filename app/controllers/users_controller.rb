ed83363cddaea0b133549aea0a712d521c19689f
Claudevan <claudevan.ti@mail.com>
2016-08-06 12:13:27 -0300

    User register yourself

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      flash[:error] = "Todos os campos são obrigatórios"
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :phone, :company, :position)
+  end
+
+end
